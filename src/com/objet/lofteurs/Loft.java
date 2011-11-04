000063308988364cd86205b9fa3f0eed1b3a322d
Nicolas <nicolas.bouillon@eleves.ec-nantes.fr>
2011-11-04 11:31:35 +0100

    new class Loft

---
+++ src/com/objet/lofteurs/Loft.java
@@ -0,0 +1,28 @@
+package com.objet.lofteurs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Loft {
+
+	private ZoneGraphique zone;
+	private int h;
+	private int w;
+	private ArrayList<ArrayList<Cellule>> listeCellules;
+	private ArrayList<Erratique> listeNeuneus;
+	
+
+	public Loft(int taille, ZoneGraphique zone){
+		this.h=taille;
+		this.w=taille;
+		this.zone=zone;
+	}
+	
+	public void remplissageAleatoire(float f){
+		
+		
+		
+	}
+	
+	
+}
