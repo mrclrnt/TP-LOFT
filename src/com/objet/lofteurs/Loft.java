8103ef60a6a9c8c4301da952f4a0ee9c59c6418c
Nicolas <nicolas.bouillon@eleves.ec-nantes.fr>
2011-11-04 11:51:42 +0100

    Loft constructor creating an array list of Cellules

--- src/com/objet/lofteurs/Loft.java
+++ src/com/objet/lofteurs/Loft.java
@@ -12,10 +12,20 @@ public class Loft {
 	private ArrayList<Erratique> listeNeuneus;
 	
 
+	//Constructeur de Loft (taille et liste de cellules)
 	public Loft(int taille, ZoneGraphique zone){
-		this.h=taille;
-		this.w=taille;
+		h=taille;
+		w=taille;
 		this.zone=zone;
+		listeCellules = new ArrayList<ArrayList<Cellule>>();
+		for(int i=0;i<w;i++){
+			ArrayList<Cellule> e = new ArrayList<Cellule>();
+			for(int j=0;j<w;j++){
+				Cellule c= new Cellule(i,j);
+				e.add(c);
+			}
+			listeCellules.add(e);
+		}
 	}
 	
 	public void remplissageAleatoire(float f){
